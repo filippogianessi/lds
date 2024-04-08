a861ebea81561382686a125db4a59a9401a3ce6d
Filippo Gianessi <gianessi.filippo@gmail.com>
2024-04-08 23:44:10 +0200

    oyl

--- src/myCard.js
+++ src/myCard.js
@@ -1,8 +1,11 @@
 
 
 import {LitElement, html} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+import myStyles from '../dist/output.css';
+
 
 export class myCard extends LitElement {
+  static styles = css([myStyles]);
   render(){
     return html`<div class="md:p-10 sm:p-6 p-6 flex md:gap-4 sm:gap-2 gap-2 items-start rounded-3xl bg-gradient-to-b from-white to-neutral-100">Hello from my template.</div>`;
   }
