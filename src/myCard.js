ed8f8030ddbcf44fc451c214abadea9efd127f7a
Filippo Gianessi <gianessi.filippo@gmail.com>
2024-04-11 00:19:14 +0200

    prova

--- src/myCard.js
+++ src/myCard.js
@@ -2,14 +2,12 @@ import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core
 
 // Assume `myStyles` is a string containing your CSS rules.
 const myStyles = css`
-  /* Your Tailwind CSS extracted for this component */
   .card {
     display: flex;
     padding: 1.5rem;
     gap: 0.5rem;
     align-items: flex-start;
     border-radius: 1.5rem;
-    background-image: background-image: linear-gradient(to bottom, var(--tw-gradient-stops));
     background-color: #ffffff;
 
     @media (min-width: 640px) {
@@ -18,16 +16,17 @@ const myStyles = css`
      }
     @media (min-width: 768px) {
       padding: 2.5rem;
-    gap: 1rem;
+      gap: 1rem;
      }
    }
 `;
 
 export class MyCard extends LitElement {
   static styles = [myStyles];
+  @property() body: string = '';
 
   render() {
-    return html`<div class="card">Hello from my template.</div>`;
+    return html`<div class="card">${this.body}</div>`;
   }
 }
 
